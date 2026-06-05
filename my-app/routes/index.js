c5ea7c277f745becd6d4636bdb3568c8274962e4
Nicolly <nicollysousace@gmail.com>
2026-06-05 15:48:18 -0300

    Merge branch 'Feature/componets' into Feature/home_screen e corrigir cardGame

--- my-app/routes/index.js
+++ my-app/routes/index.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Home' });
+  res.render('index', { 
+    title: 'Home',
+    exclusiveCSS: 'home.css' 
+  });
 });
 
 module.exports = router;
