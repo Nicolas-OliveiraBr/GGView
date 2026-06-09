be205334baf1903cbf16219fa01f3424c8da3f2e
Nicolly <nicollysousace@gmail.com>
2026-06-08 21:45:35 -0300

    Ajustando rotas, componente de review e card do perfil e estilos

--- my-app/app.js
+++ my-app/app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var gameRouter = require('./routes/game_reviews_screen');
+var profileRouter = require('./routes/profile'); 
 
 var app = express();
 
@@ -23,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/games', gameRouter);
+app.use('/profile', profileRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- my-app/routes/profile.js
+++ my-app/routes/profile.js
@@ -2,15 +2,31 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-  res.render('games', {
+  res.render('profile', {
     title: `Profile`,
     exclusiveCSS: 'profile.css' 
   }) 
 })
 
-router.get('/profile/:username', function(req, res) {
+router.get('/:username', function(req, res) {
+    const fakeReviews = [
+      { author: 'Nicolas', nameGame: 'The Witcher 3', year: 2015, rating: 5, gameCover: '/images/imgcarousel.jpg' },
+      { author: 'Lucas', nameGame: 'Elden Ring', year: 2022, rating: 4, gameCover: '/images/imgcarousel2.jpg' },
+      { author: 'Nicolas', nameGame: 'The Witcher 2', year: 2010, rating: 5, gameCover: '/images/imgcarousel.jpg' },
+    ];
+    const authorDescription = "Gamer and reviewer";
+
     var user = req.params.username;
-    res.render('profile', { name: user }); // Renderiza a sua tela de perfil
+
+    const userReviews = fakeReviews.filter(review => review.author === user);
+
+    res.render('profile', {
+        title: `Perfil de ${user}`,
+        authorName: user,
+        exclusiveCSS: 'profile.css',
+        fakeReviews: userReviews,
+        authorDescription: authorDescription
+    });
 });
 
 module.exports = router;
