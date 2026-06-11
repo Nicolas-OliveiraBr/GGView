e5292d2d10ad4fb1b351c5a42ba9bde9990c144e
SalosFB <samuel.cfb102009@gmail.com>
2026-06-11 01:37:44 -0300

    api.ts: routers de review da api criados

--- my-app/routes/api.ts
+++ my-app/routes/api.ts
@@ -2,8 +2,8 @@
 import { Router, Request, Response, NextFunction } from "express";
 import UserDAO, {IUser} from "../DAOs/UserDAO";
 import ReviewDAO, {IReview} from "../DAOs/ReviewDAO";
-import jogoDAO, {IJogo} from "../DAOs/JogoDAO";
-import { Collection, MongoClient } from "mongodb";
+import JogoDAO, {IJogo} from "../DAOs/JogoDAO";
+import { Collection, MongoClient, ObjectId } from "mongodb";
 import * as bcrypt from "bcrypt"
 
 //peso do hash de senha
@@ -64,7 +64,7 @@ router.get("/jogo/get/:id_igdb", async (req: Request, res: Response, next: NextF
         if(!id_igdb) {
             return res.status(400).json({error: "Id de jogo não fornecida"})
         }
-        const resultado = await jogoDAO.getGame(jogoColl, String(id_igdb));
+        const resultado = await JogoDAO.getGame(jogoColl, String(id_igdb));
 
         if (resultado && 'status' in resultado) {
             return res.status(resultado.status).json(resultado.body);
@@ -84,7 +84,7 @@ router.put("/jogo/put/plusonetocounter/:id_igdb", async (req: Request, res: Resp
             return res.status(400).json({error: "Id de jogo não fornecida"})
         }
         const { counter } = req.body;
-        const resultado = await jogoDAO.plusOneToCounter(jogoColl, String(id_igdb), counter)
+        const resultado = await JogoDAO.plusOneToCounter(jogoColl, String(id_igdb), counter)
 
         if (resultado && 'status' in resultado) {
             return res.status(resultado.status).json(resultado.error);
@@ -104,7 +104,7 @@ router.put("/jogo/put/minusonetocounter/:id_igdb", async (req: Request, res: Res
             return res.status(400).json({error: "Id de jogo não fornecida"})
         }
         const { counter } = req.body;
-        const resultado = await jogoDAO.plusOneToCounter(jogoColl, String(id_igdb), counter)
+        const resultado = await JogoDAO.plusOneToCounter(jogoColl, String(id_igdb), counter)
 
         if (resultado && 'status' in resultado) {
             return res.status(resultado.status).json(resultado.error);
@@ -112,8 +112,58 @@ router.put("/jogo/put/minusonetocounter/:id_igdb", async (req: Request, res: Res
 
         return res.status(200).json({ message: "Contador decrementado com sucesso.", resultado });
     } catch(err) {
-        console.error("Erro na rota /jogo/plusonetocounter:", err);
+        console.error("Erro na rota /jogo/minusonetocounter:", err);
         return res.status(500).json({ error: "Erro interno na rota do servidor." });
     }
 })
+
+router.post("/review/post", async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const { review } = req.body;
+        
+        if (!review || typeof review !== 'object') {
+            return res.status(400).json({ error: "O campo 'review' deve ser um objeto válido." });
+        }
+        
+        const resultado = await ReviewDAO.criarReview(reviewColl, jogoColl, review)
+
+        return res.status(200).json({ message: "Review postada com sucesso.", resultado });
+    } catch(err) {
+        console.error("Erro na rota /review/post:", err);
+        return res.status(500).json({ error: "Erro interno na rota do servidor." });
+    }
+});
+
+router.put("/review/put/:id_igdb/:id_user", async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const { changes } = req.body;
+        const { id_user, id_igdb } = req.params;
+        
+        if (!changes || typeof changes !== 'object') {
+            return res.status(400).json({ error: "O campo 'review' deve ser um objeto válido." });
+        }
+        const id_userobj = new ObjectId(String(id_user))
+
+        const resultado = await ReviewDAO.editarReview(reviewColl, id_userobj, String(id_igdb), changes)
+        return res.status(200).json({ message: "Review atualizada com sucesso.", resultado });
+    } catch(err) {
+        console.error("Erro na rota /review/put:", err);
+        return res.status(500).json({ error: "Erro interno na rota do servidor." });
+    }
+});
+
+router.get("/review/get/gameReviews/:id_igdb", async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const { id_igdb } = req.params;
+        if(!id_igdb) {
+            return res.status(400).json({error: "Id de jogo não fornecida"})
+        }
+        
+        const resultado = await ReviewDAO.getGameReviews(reviewColl, String(id_igdb))
+        return res.status(200).json({ resultado });
+    } catch(err) {
+        console.error("Erro na rota /review/get/gameReviews:", err);
+        return res.status(500).json({ error: "Erro interno na rota do servidor." });
+    }
+});
 export default router;
